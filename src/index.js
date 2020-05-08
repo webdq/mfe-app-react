20eb829b83b20039bb401076afb8bffc38b635d0
webdq <d312697510@126.com>
2020-05-08 17:16:55 +0800

    添加react子应用

--- src/index.js
+++ src/index.js
@@ -3,13 +3,49 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './public-path';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function render(props) {
+  const { container } = props;
+  ReactDOM.render(
+    <App />,
+    container
+      ? container.querySelector('#root')
+      : document.querySelector('#root')
+  );
+}
+
+function storeTest(props) {
+  props.onGlobalStateChange(
+    (value, prev) =>
+      console.log(`[onGlobalStateChange - ${props.name}]:`, value, prev),
+    true
+  );
+  props.setGlobalState({
+    ignore: props.name,
+    user: {
+      name: props.name,
+    },
+  });
+}
+
+if (!window.__POWERED_BY_QIANKUN__) {
+  render({});
+}
+
+export async function bootstrap() {
+  console.log('[react16] react app bootstraped');
+}
+
+export async function mount(props) {
+  console.log('[react16] props from main framework', props);
+  storeTest(props);
+  render(props);
+}
+
+export async function unmount() {
+  ReactDOM.unmountComponentAtNode(document.getElementById('root'));
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
